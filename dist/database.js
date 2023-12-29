59560dc75d369cbd7252d71e852c76f36a1b2626
Taylor <taylor.merwin@gmail.com>
2023-12-28 18:10:08 -0800

    built js files

--- dist/database.js
+++ dist/database.js
@@ -3,16 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getPosts = exports.getPost = void 0;
-const mysql2_1 = __importDefault(require("mysql2"));
+exports.getPosts = exports.getPostPreview = exports.getPost = void 0;
+const promise_1 = __importDefault(require("mysql2/promise"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const pool = mysql2_1.default.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-}).promise();
+const pool = promise_1.default.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+    ssl: {
+        rejectUnauthorized: true
+    }
+});
 async function getPost(postID) {
     const [row] = await pool.query(`
   SELECT *
@@ -21,9 +24,31 @@ async function getPost(postID) {
     return [row];
 }
 exports.getPost = getPost;
+async function getPostPreview(postID) {
+    const [row] = await pool.query(`
+  SELECT 
+    BlogPosts.title, 
+    BlogPosts.post_description, 
+    BlogPosts.created_at, 
+    Users.username AS author_name
+  FROM 
+    BlogPosts 
+  INNER JOIN 
+    Users ON BlogPosts.author_id = Users.user_id
+  WHERE 
+    BlogPosts.post_id = ?`, [postID]);
+    return row;
+}
+exports.getPostPreview = getPostPreview;
 async function getPosts() {
-    const [rows] = await pool.query(`
-  SELECT * FROM BlogPosts`);
-    return rows;
+    try {
+        const [rows] = await pool.query(`
+    SELECT * FROM BlogPosts`);
+        return rows;
+    }
+    catch (error) {
+        console.error(error);
+        throw error; // re-throw the error so it can be handled by your error handling middleware
+    }
 }
 exports.getPosts = getPosts;
--- dist/server.js
+++ dist/server.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const database_js_1 = require("./database.js");
+const database_1 = require("./database");
 const app = (0, express_1.default)();
 const port = 8080;
 app.set("view engine", "ejs");
@@ -12,14 +12,32 @@ app.set("view engine", "ejs");
 app.use(express_1.default.static('public'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/', async (req, res) => {
+    try {
+        const post = await (0, database_1.getPostPreview)('1');
+        res.render('index', { post });
+        console.log(post);
+    }
+    catch (error) {
+        res.status(500).send('Error fetching post');
+    }
 });
 // get a post by id
 app.get('/blogPost/:post_id', async (req, res) => {
     try {
         const id = req.params.post_id;
-        const post = await (0, database_js_1.getPost)(id);
+        const post = await (0, database_1.getPost)(id);
+        res.json(post); // Use res.json to send JSON response
+    }
+    catch (error) {
+        res.status(500).send('Error fetching post');
+    }
+});
+// get a post preview by id
+app.get('/postPreview/:post_id', async (req, res) => {
+    try {
+        const id = req.params.post_id;
+        const post = await (0, database_1.getPostPreview)(id);
         res.json(post); // Use res.json to send JSON response
     }
     catch (error) {
@@ -28,11 +46,15 @@ app.get('/blogPost/:post_id', async (req, res) => {
 });
 // get all posts
 app.get('/blogPosts/', async (req, res) => {
+    console.log('Handling GET /blogPosts/ request...');
     try {
-        const posts = await (0, database_js_1.getPosts)();
-        res.json(posts); // Use res.json to send JSON response
+        console.log('Calling getPosts...');
+        const posts = await (0, database_1.getPosts)();
+        console.log('getPosts returned, sending response...');
+        res.send(posts);
     }
     catch (error) {
+        console.error('Error in GET /blogPosts/ handler:', error);
         res.status(500).send('Error fetching posts');
     }
 });
