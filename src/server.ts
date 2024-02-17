11e18c0bc0db67f0815440fdb134a2167e3f1b60
Taylor Merwin <taylor.merwin@gmail.com>
2024-02-16 16:46:41 -0800

    images now appear properly

--- src/server.ts
+++ src/server.ts
@@ -34,7 +34,7 @@ app.set("view engine", "ejs");
 
 
 app.use(express.static('public'));
-app.use('/uploads', express.static('uploads'));
+app.use(express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(session({
