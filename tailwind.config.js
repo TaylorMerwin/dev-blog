1b8f0f15fe3377ba353ee7e35e73bf8820f7f417
Taylor Merwin <taylor.merwin@gmail.com>
2024-04-01 14:11:13 -0700

    using tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/*.ejs"],
-  theme: {
-    extend: {},
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+
+  daisyui: {
+    themes: ["bumblebee", "dim"],
   },
-  plugins: [
-    {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  ],
 };
